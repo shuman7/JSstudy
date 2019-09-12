7570dcd8d12220c776b952489de57e96c3bcfc05
shuman7 <shuman1231@gmail.com>
2019-09-12 09:17:11 +0900

    5-03追記

--- 5-02_location/script.js
+++ 5-02_location/script.js
@@ -1,3 +1,29 @@
-document.getElementById('form').querySelector.onchange = function() {
-    location.href = document.getElementById('form').querySelector.value;
+var lang = document.querySelector('html').lang;
+
+var opt;
+if(lang === 'ja') {
+    opt = document.querySelector('option[value="index.html"]');
+} else if(lang === 'en') {
+    opt = document.querySelector('option[value="index-en.html"]');
+} else if(lang === 'zh') {
+    opt = document.querySelector('option[value="index-zh.html"]');    
+}
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function() {
+    location.href = document.getElementById('form').select.value;
+}
+
+switch (lang) {
+    case lang === ja:
+        opt = document.querySelector('option[value="index.html"]')
+        break;
+    case lang === en:
+        opt = document.querySelector('option[value="index-en.html"]')
+        break;
+    case lang === ja:
+        opt = document.querySelector('option[value="index-zh.html"]')
+        break;
+
+
 }
